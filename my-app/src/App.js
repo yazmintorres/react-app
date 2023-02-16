0c7a7ec5250142ca64d0f36a7cafb2e1be69e08c
Yazmin <yazminmoniquetorres@gmail.com>
2023-02-16 11:02:25 -0800

    Add main contents and divide sections

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,30 @@
 // this is my parent component that react will render
 
 export default function App() {
-  return <h1>Test</h1>;
+  return (
+    <div id="body-container">
+      <section id="intro">
+        <h1>Hello</h1>
+        <p>
+          Instructions: guess the number between 1 and 20. You only have 5
+          chances!
+        </p>
+      </section>
+      <section id="game">
+        <button id="generate-number">Generate random number</button>
+        <p>Message: too high or too low</p>
+        <div id="container">
+          <div class="user-cards">
+            <p>Guess Number</p>
+            <input type="text"></input>
+            <button>Submit</button>
+          </div>
+          <div class="user-cards">
+            <p>Chances Left</p>
+            <div>Input number of chances left</div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
