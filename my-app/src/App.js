a5dfdbdb3596e2610e0c67b264765ed59b095dac
Yazmin <yazminmoniquetorres@gmail.com>
2023-02-16 13:37:54 -0800

    Make Game component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,13 @@
 // this is my parent component that react will render
 import "./styles.css";
 import Intro from "./components/Intro";
+import Game from "./components/Game";
 
 export default function App() {
   return (
     <div id="body-container">
       <Intro />
-      <section id="game">
-        <div id="secret-number">
-          <p>Secret Number:</p>
-          <button id="generate-number">Click Me</button>
-        </div>
-        <p>Message: too high or too low</p>
-        <div id="container">
-          <div class="user-cards">
-            <p>Guess Number</p>
-            <form>
-              <input type="text"></input>
-              <button id="submit" type="submit">
-                Submit
-              </button>
-            </form>
-          </div>
-          <div class="user-cards">
-            <p>Chances Left</p>
-            <div id="chances">5</div>
-          </div>
-        </div>
-      </section>
+      <Game />
     </div>
   );
 }
---
+++ my-app/src/components/Game.js
@@ -0,0 +1,26 @@
+export default function Game() {
+  return (
+    <section id="game">
+      <div id="secret-number">
+        <p>Secret Number:</p>
+        <button id="generate-number">Click Me</button>
+      </div>
+      <p>Message: too high or too low</p>
+      <div id="container">
+        <div class="user-cards">
+          <p>Guess Number</p>
+          <form>
+            <input type="text"></input>
+            <button id="submit" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+        <div class="user-cards">
+          <p>Chances Left</p>
+          <div id="chances">5</div>
+        </div>
+      </div>
+    </section>
+  );
+}
