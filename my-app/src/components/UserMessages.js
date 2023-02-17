93499bacc1d23cd39cc67537f8d0747c71f98487
Yazmin <yazminmoniquetorres@gmail.com>
2023-02-17 14:53:04 -0800

    fixed test cases

--- my-app/src/components/UserMessages.js
+++ my-app/src/components/UserMessages.js
@@ -33,25 +33,38 @@ export default function UserMessages() {
     e.preventDefault();
     console.log(attempts);
     let inputValue = e.target.guess.value;
-    if (attempts > 0) {
-      if (secretNum > inputValue) {
-        setMessage(<p style={{ color: "red" }}>Too low!</p>);
-      } else if (secretNum < inputValue) {
-        setMessage(<p style={{ color: "red" }}>Too high!</p>);
-      } else if (secretNum === Number(inputValue)) {
-        setMessage(<p style={{ color: "green" }}>Correct!</p>);
+    let gameOn = attempts > 0;
+    if (gameOn) {
+      if (secretNum === Number(inputValue)) {
+        setMessage(
+          <>
+            <p style={{ color: "green" }}>Correct!</p>
+            <p style={{ color: "green" }}>Game Over</p>
+          </>
+        );
         setDisplay(displaySecretNumEl);
+        gameOn = false;
+      } else {
+        setAttempts((prevValue) => prevValue - 1);
+        if (attempts === 1) {
+          console.log(inputValue, secretNum, inputValue !== secretNum);
+          setMessage(
+            <>
+              <p style={{ color: "purple" }}>Uh oh, no more chances</p>
+              <p style={{ color: "purple" }}>Game Over</p>
+            </>
+          );
+          setDisplay(displaySecretNumEl);
+          gameOn = false;
+        } else {
+          if (secretNum > inputValue) {
+            setMessage(<p style={{ color: "red" }}>Too low!</p>);
+          } else if (secretNum < inputValue) {
+            setMessage(<p style={{ color: "red" }}>Too high!</p>);
+          }
+        }
       }
-
-      if (attempts === 1 && Number(inputValue) !== secretNum) {
-        console.log(inputValue, secretNum, inputValue !== secretNum);
-        setMessage(<p style={{ color: "purple" }}>Uh oh, no more chances</p>);
-        setDisplay(displaySecretNumEl);
-        setAttempts(0);
-      }
-      setAttempts((prevValue) => prevValue - 1);
     }
-
     console.log(`guess: ${inputValue}, secret Number: ${secretNum} `);
   }
 
