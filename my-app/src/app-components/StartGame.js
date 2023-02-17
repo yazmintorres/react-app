fac16861b3c70539f4b90198d7e666779e0486c5
Yazmin <yazminmoniquetorres@gmail.com>
2023-02-16 17:45:32 -0800

    Add question mark image to #secretnumber element

--- my-app/src/app-components/StartGame.js
+++ my-app/src/app-components/StartGame.js
@@ -3,6 +3,9 @@ import UserMessage from "../startgame-components/UserMessage";
 
 export default function StartGame() {
   const [secretNumber, setSecretNumber] = useState(null);
+  const [message, setMessage] = useState(
+    <img src="https://cdn-icons-png.flaticon.com/512/3524/3524335.png" />
+  );
   function handleClick() {
     setSecretNumber(Math.floor(Math.random() * 20 + 1));
     console.log(secretNumber);
@@ -11,9 +14,7 @@ export default function StartGame() {
     <section id="game" className="container">
       <div id="secret-number-container">
         <p>Secret Number:</p>
-        <button id="secret-number" onClick={handleClick}>
-          Click Me
-        </button>
+        <div id="secret-number">{message}</div>
       </div>
       <UserMessage secretNumber={secretNumber} />
     </section>
