e49a05b15b0108dbb296af9971852226d7277e93
Yazmin <yazminmoniquetorres@gmail.com>
2023-02-16 17:56:21 -0800

    Create secretNumber state

--- my-app/src/app-components/StartGame.js
+++ my-app/src/app-components/StartGame.js
@@ -2,14 +2,14 @@ import { useState } from "react";
 import UserMessage from "../startgame-components/UserMessage";
 
 export default function StartGame() {
-  const [secretNumber, setSecretNumber] = useState(null);
+  const [secretNumber, setSecretNumber] = useState(() =>
+    Math.floor(Math.random() * 20 + 1)
+  );
+
   const [message, setMessage] = useState(
     <img src="https://cdn-icons-png.flaticon.com/512/3524/3524335.png" />
   );
-  function handleClick() {
-    setSecretNumber(Math.floor(Math.random() * 20 + 1));
-    console.log(secretNumber);
-  }
+
   return (
     <section id="game" className="container">
       <div id="secret-number-container">
